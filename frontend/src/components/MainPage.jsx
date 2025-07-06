2227bba3c8734d19f66e4586e4f87a3933f3f00e
neformal2005 <robert.ogannisian2015@yandex.ru>
2025-07-06 12:30:56 +0300

    Добавлена карта на сайт с отображажением меток. Метки пока что хранятся в папке data в js-файле.

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,4 +1,5 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import MapComponent from './MapComponent';
 
 function MainPage({ user }) {
   const navigate = useNavigate();
@@ -6,19 +7,21 @@ function MainPage({ user }) {
   return (
     <div
       style={{
-        textAlign: "center",
-        color: "green",
-        fontSize: "24px",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100%",
-        width: "100%",
-        padding: "20px",
+        textAlign: 'center',
+        color: 'green',
+        fontSize: '24px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        width: '100%',
+        padding: '20px',
       }}
     >
-      <h1>Привет, {user?.name || user?.login || "Гость"}!</h1>
+      <h1>Привет, {user?.name || user?.login || 'Гость'}!</h1>
+      <p>Путешествуй с нами!</p>
+      <MapComponent />
     </div>
   );
 }
---
+++ frontend/src/data/VolgogradPlaces.js
@@ -0,0 +1,18 @@
+// Пример туристических мест Волгограда (координаты в [longitude, latitude])
+// Пока делаем так, далее свяжу с бд
+const volgogradPlaces = [
+  {
+    name: "Мамаев Курган",
+    coordinates: [44.5370, 48.7422],
+  },
+  {
+    name: "Музей-панорама 'Сталинградская битва'",
+    coordinates: [44.5304, 48.7164],
+  },
+  {
+    name: "Дом Павлова",
+    coordinates: [44.5311, 48.7156],
+  },
+];
+
+export default volgogradPlaces;
